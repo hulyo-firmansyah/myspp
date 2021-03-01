60d75dedd4f6b79e24563342f5d5408b789b281d
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-01 11:26:43 +0700

    membuat view login dan register

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,84 @@
+@extends('_partials.master')
+@section('lib_css')
+<link rel="stylesheet" href="/modules/bootstrap-social/bootstrap-social.css">
+@endsection
+@section('content')
+<section class="section">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                <div class="login-brand">
+                    <img src="/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                </div>
+
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h4>Login</h4>
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login.process') }}" class="needs-validation" novalidate="">
+                            @csrf
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" type="text" class="form-control" value="{{old('username')}}" name="username" tabindex="1" required
+                                    autofocus>
+                                @if ($errors->has('username'))
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <div class="d-block">
+                                    <label for="password" class="control-label">Password</label>
+                                    <div class="float-right">
+                                        <a href="auth-forgot-password.html" class="text-small">
+                                            Forgot Password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <input id="password" type="password" class="form-control" name="password" tabindex="2"
+                                    required>
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" name="remember" class="custom-control-input" tabindex="3"
+                                        id="remember-me">
+                                    <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="simple-footer">
+                    Copyright &copy; Stisla <script>document.write(new Date().getFullYear())</script>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+@section('js_lib')
+<script src="/modules/sweetalert/sweetalert.min.js"></script>
+@endsection
+@section('js_custom')
+@if (session('error'))
+    <script>
+        swal("{{ session('error') }}", 'Harap periksa username dan password!', 'error');
+    </script>
+@endif
+@endsection
