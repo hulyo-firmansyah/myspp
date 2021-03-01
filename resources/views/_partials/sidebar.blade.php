d90207c7ebea0a27a37c63198b89cc820d02a724
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-01 12:43:36 +0700

    membuat menu sidebar(kemungkinan akan dirubah)

--- resources/views/_partials/sidebar.blade.php
+++ resources/views/_partials/sidebar.blade.php
@@ -11,5 +11,23 @@
             <li class="dropdown active">
                 <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
+            <li class="menu-header">Master</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-database"></i><span>Master</span></a>
+                <ul class="dropdown-menu has-dropdown">
+                    <li><a href="#" class="nav-link"><i class="fas fa-users"></i><span>Petugas</span></a></li>
+                    <li><a href="#" class="nav-link"><i class="fas fa-users"></i><span>Siswa</span></a></li>
+                    <li><a href="#" class="nav-link"><i class="fas fa-users"></i><span>Kelas</span></a></li>
+                    <li><a href="#" class="nav-link"><i class="fas fa-users"></i><span>SPP</span></a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Transaksi</li>
+            <li class="dropdown has-dropdown">
+                <a href="#" class="nav-link"><i class="fas fa-book"></i><span>Transaksi</span></a>
+            </li>
+            <li class="menu-header">Laporan</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link"><i class="fas fa-chart-bar"></i><span>Laporan</span></a>
+            </li>
     </aside>
 </div>
