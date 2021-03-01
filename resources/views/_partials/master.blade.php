eeec47c884d8f059b0c398e0ba157cdc7404789d
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-01 11:23:37 +0700

    membuat base view partials

---
+++ resources/views/_partials/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('_partials.head')
+
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+
+    @include('_partials.script')
+</body>
+
+</html>
