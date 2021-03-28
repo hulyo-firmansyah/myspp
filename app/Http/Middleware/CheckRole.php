2b9c616adc0415d750261e9d9ac8eccf77fbc38e
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 21:50:22 +0700

    Membuat memperbaiki middleware checkrole agar optimal

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,27 +16,10 @@ class CheckRole
      * @return mixed
      */
     public function handle($request, Closure $next, $role1, $role2=null)
-    {   
-        // $guard = Main::getActiveGuard();
-        // dd(Auth::guard('admin')->check(), $request, Auth::getDefaultDriver(), Auth::guard($guard)->check());
-        // if(Auth::guard($role)->check()){
-        //     if(Auth::guard($role)->user()->role === $role){
-        //         return $next($request);
-        //     }
-        //     return abort(404);
-        // }
-
-        // if(Auth::user()->role == $role){
-        //     return $next($request);
-        // }
-        // return abort(404);
-        // return redirect()->intended(route('login.form'));
-        
+    { 
         if(auth()->check()){
-            if(auth()->user()->role == $role1 || auth()->user()->role == $role=2) return $next($request);
+            if(auth()->user()->role == $role1 || auth()->user()->role == $role2) return $next($request);
             return abort(403);
         }
-        // abort_unless(auth()->check() && auth()->user()->role == $role, 403);
-        // return $next($request);
     }
 }
