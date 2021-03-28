9d7c735ca0c12fa7b4b73dbf08b078df577cea4d
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 21:59:42 +0700

    Membuat stepModel

---
+++ app/StepsModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StepsModel extends Model
+{
+    protected $table = 'tingkatan';
+    protected $primaryKey = 'id_tingkatan';
+}
