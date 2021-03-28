95c3d880db1538802c16c820cd916426e4909750
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 22:10:38 +0700

    Mengubah data pada footer agar dinamis

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,6 +1,10 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        Copyright &copy; <script>
+            document.write(new Date().getFullYear())
+
+        </script>
+        <div class="bullet"></div> By <a href="{{Config::get('site_vars.social.INSTAGRAM')}}">{{ Config::get('site_vars.app_name') }}</a>
     </div>
     <div class="footer-right">
 
