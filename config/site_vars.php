5d93d0be515a2a29a7283c71410457d83b400bb6
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 21:57:37 +0700

    Membuat variable global pada config/site_vars.php

---
+++ config/site_vars.php
@@ -0,0 +1,19 @@
+<?php
+return[
+    'role' => [
+        'FIRST' => 'admin',
+        'SECOND' => 'worker',
+        'THIRD' => 'student'
+    ],
+    'app_name' => 'My SPP',
+    'social' => [
+        'INSTAGRAM' => 'http://www.instagram.com/alex_nosyura',
+        'FACEBOOK' => 'https://www.facebook.com/profile.php?id=100010819697444'
+    ],
+    'steps' => [
+        '10' => 'X',
+        '11' => 'XI',
+        '12' => 'XII',
+        '13' => 'XIII'
+    ]
+];
