41cffb216fe5ae0013fdc16698dfea463a5eee7e
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 22:20:28 +0700

    Membuat view dashboard untuk siswa

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,96 @@
+@extends('_partials.master')
+@section('css_lib')
+<link rel="stylesheet" href="/modules/datatables/datatables.min.css">
+<link rel="stylesheet" href="/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
+<link rel="stylesheet" href="/modules/izitoast/css/iziToast.min.css">
+@endsection
+@section('css_custom')
+<style>
+    #dataTablesTopContainer {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 30px;
+    }
+
+</style>
+@endsection
+@section('content')
+<div class="section-header">
+    <h1>Dashboard Siswa</h1>
+</div>
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-primary">
+                <i class="far fa-user"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>Total Admin</h4>
+                </div>
+                <div class="card-body">
+                    10
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-danger">
+                <i class="far fa-newspaper"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>News</h4>
+                </div>
+                <div class="card-body">
+                    42
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-warning">
+                <i class="far fa-file"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>Reports</h4>
+                </div>
+                <div class="card-body">
+                    1,201
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-success">
+                <i class="fas fa-circle"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>Online Users</h4>
+                </div>
+                <div class="card-body">
+                    47
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('js_lib')
+<script src="/modules/datatables/datatables.min.js"></script>
+<script src="/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<script src="/modules/datatables/Select-1.2.4/js/dataTables.select.min.js"></script>
+<script src="/modules/jquery-ui/jquery-ui.min.js"></script>
+<script src="/modules/sweetalert/sweetalert.min.js"></script>
+<script src="/modules/izitoast/js/iziToast.min.js"></script>
+<script src="/modules/input-mask/jquery.inputmask.bundle.min.js"></script>
+<script src="/modules/blockui/jquery.blockUI.min.js"></script>
+@endsection
+@section('js_custom')
+@endsection
