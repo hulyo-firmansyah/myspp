ee7ca0f68ecbce98071d88022a2c76264377ee02
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-03-28 22:04:25 +0700

    HomeController admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \Auth;
+use App\Helpers\Main;
 
 class HomeController extends Controller
 {
